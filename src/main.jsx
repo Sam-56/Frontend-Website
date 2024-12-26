2ad15b83f4fb2926d4ba57c06268ae69a3f72d29
Sam <samarthsrivastava824@gmail.com>
2024-12-27 00:40:54 +0530

    Frontend

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './components/Layout/Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+
+const router= createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children: [
+      {
+      path:"",
+      element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router}/>
+  </StrictMode>,
+)
